13bd77c8fa700f6e84a535bb0b9ae73ef098b777
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2020-02-23 17:19:31 +0530

    make it better

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ MyBrary
 
 Project made by node express and mongo.db for stack practice
 
+using heroku
+https://dashboard.heroku.com/apps/mybrary-nd/deploy/heroku-git
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+
 const express=require('express');
 const app=express();
 const expressLayouts=require('express-ejs-layouts');
@@ -12,7 +18,7 @@ app.use(express.static('public'));
 
 const mongoose= require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true});
+    useNewUrlParser:true,useUnifiedTopology:true});
 const db=mongoose.connection;
 db.on('error',error=>console.error(error));
 db.once('open',()=>console.log('Connected to Mongoose'));
